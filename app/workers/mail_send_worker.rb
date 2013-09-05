65c7bf8ca7022c2c4db071d5287e4e6c0b34b8de
krongk <kenrome@gmail.com>
2013-09-05 23:10:39 +0800

    Repair mail send

--- app/workers/mail_send_worker.rb
+++ app/workers/mail_send_worker.rb
@@ -2,33 +2,32 @@ class MailSendWorker
   include Sidekiq::Worker
 
   def perform(mail_tmp_id, mail_item_ids)
-    puts "............"
-    puts mail_tmp_id
-    puts mail_item_ids
-    email_tmp = MailTmp.find_by_id(mail_tmp_id)
-    return email_tmp.nil? || mail_item_ids.empty?
+    mail_tmp = MailTmp.find(mail_tmp_id)
+    return if mail_tmp.nil? || mail_item_ids.empty?
 
     #fetch from email
-    from_email = current_user.user_detail.domain
-    from_email = 'admin@' + current_user.user_detail.domain unless from_email.nil?
+    current_user = User.find(mail_tmp.user_id)
+    from_email = current_user.user_detail.website
+    unless from_email.nil?
+      from_email = 'admin@' + from_email.sub(/^http(s)?(:)?(\/\/)?(www)?(\.)?/i, '')
+    end
     from_email ||= current_user.email
-
-    puts "start....."
+    
     MailItem.where(:id => mail_item_ids).each do |item|
       current_user = User.find(item.user_id)
 
       puts "#{from_email} => #{item.email} start"
-      UserMailer.marketing(email_tmp, from_email, item.email).deliver
+      UserMailer.marketing(mail_tmp, from_email, item.email).deliver
      
       status = 'y'
-      item.is_processed = item.is_processed == 'n' ? "#{email_tmp.id},#{status}" : "#{email_tmp.id},#{status}|" + item.is_processed
+      item.is_processed = item.is_processed == 'n' ? "#{mail_tmp.id},#{status}" : "#{mail_tmp.id},#{status}|" + item.is_processed
       item.save!
       
       billing_count = 1
       MailLog.create!(
           :user_id => item.user_id, 
           :mail_item_id => item.id, 
-          :mail_tmp_id => email_tmp.id, 
+          :mail_tmp_id => mail_tmp.id, 
           :status => status,
           :billing_count => billing_count)
 
