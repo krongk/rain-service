1e4d1342bca983f7118162cd547b586c034092c3
krongk <kenrome@gmail.com>
2013-09-17 20:28:12 +0800

    Add mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 class UserMailer < ActionMailer::Base
-  default from: "雨服务@yufuwu.cn"
+  default from: "834700418@qq.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
