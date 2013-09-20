68c26bca5225d969cb44987b27c54561d0bf7aa4
krongk <kenrome@gmail.com>
2013-09-20 16:22:40 +0800

    Add mailgun e353

--- app/mailers/qq_mailer.rb
+++ app/mailers/qq_mailer.rb
@@ -5,9 +5,9 @@ class QqMailer < ActionMailer::Base
     address: "smtp.qq.com",
     port: 25,
     authentication: "plain",
-    user_name: UserAccount.get('qmail_name'),
-    password: UserAccount.get('qmail_password'),
-    domain: UserAccount.get('domain'),
+    # user_name: UserAccount.get('qmail_name'),
+    # password: UserAccount.get('qmail_password'),
+    # domain: UserAccount.get('domain'),
   }
 
   def marketing(mail_tmp_id, to_email)
