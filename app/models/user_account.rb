e79161a27a97679b09ccbe976ebd2dd956d5d0e5
krongk <kenrome@gmail.com>
2013-09-20 16:09:24 +0800

    Add mailgun e35

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -4,10 +4,14 @@ class UserAccount < ActiveRecord::Base
   validates_presence_of :name, :value
 
   def self.get(name)
+    puts "__________________________"
+    User.current_user.id
+    puts "__________________________"
     begin
       self.where("user_id = ? and name = ?", User.current_user.id, name).first.value
-    rescue
-      ''
+    rescue => ex
+      puts ex.message
+      raise
     end
   end
 end
